222a088f44b8d5131bf3b42da4cc8e7bdfce75bd
Derek <nightwing917@hotmail.com>
2021-09-26 18:54:18 -0700

    grabbed DOM elements for now

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log('Connected!');
+const calculatorDisplay = document.querySelector('h1');
+const inputBtn = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
