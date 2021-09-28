9893d1771746974c4a598fb01c2d3d5e42488031
Derek <nightwing917@hotmail.com>
2021-09-27 20:15:19 -0700

    added send number value function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const calculatorDisplay = document.querySelector('h1');
 const inputBtn = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+   console.log(number);
+}
+
+console.log(inputBtn);
