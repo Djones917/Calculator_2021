c551c2a85cf6e8e2637d5b47e9d5f061ec854111
Derek <nightwing917@hotmail.com>
2021-09-28 19:28:44 -0700

    testing

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function sendNumberValue(number) {
 // Add Event Listeners for Numbers, Operators and Decimal
 inputBtns.forEach((inputBtn) => {
    if (inputBtn.classList.length === 0) {
-       inputBtn.addEventListener('click', sendNumberValue)
+       inputBtn.addEventListener('click', sendNumberValue(inputBtn.value));
+   } else if (inputBtn.classList.contains('operator')) {
+      inputBtn.addEventListener('click', sendNumberValue(inputBtn.value));
+   } else if (inputBtn.classList.contains('decimal')) {
+      inputBtn.addEventListener('click', sendNumberValue());
    }
 });
